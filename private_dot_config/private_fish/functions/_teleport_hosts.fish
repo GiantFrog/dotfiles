828ea32a2953d7437f39a1a6ea27cd5846b28449
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2025-09-02 13:18:46 -0700

    Make _teleport_hosts to do nothing if teleport is not installed

--- private_dot_config/private_fish/functions/_teleport_hosts.fish
+++ private_dot_config/private_fish/functions/_teleport_hosts.fish
@@ -1,11 +1,14 @@
 function _teleport_hosts
     set -f cache_file ~/.cache/teleport_server_cache.txt
-    if test -e $cache_file
+    if not command -q tsh
+        # tsh is not installed; let's do nothing.
+    else if test -e $cache_file
+        # use the old cache file
         cat $cache_file
         # update hosts cache in a non-blocking way
         nohup tsh ls --format=names > $cache_file &
     else
-        # update hosts cache now and wait for it
+        # no cache; update hosts now and wait for it
         tsh ls --format=names > $cache_file
         cat $cache_file
     end
