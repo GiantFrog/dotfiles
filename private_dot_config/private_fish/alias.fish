8f2f9b85dcc9ece152035e8f8e2122e8ed23eb08
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2025-09-28 10:56:27 -0700

    Improve fish aliases and nano highlighting

--- private_dot_config/private_fish/alias.fish
+++ private_dot_config/private_fish/alias.fish
@@ -5,7 +5,9 @@ alias la='eza -lah@ --git --icons'
 alias yaml='yq'
 
 abbr --add k kubectl
-abbr --add kns kubectl config set-context --namespace=
-abbr --add kube tsh kube
+abbr --add kns kubectl config set-context --current --namespace
+abbr --add kube tsh kube login
+abbr --add mdb tsh db connect --db-user=db-admin --db-name=mysql
+abbr --add pdb tsh db connect --db-user=db-admin --db-name=postgres
+abbr --add tflogin "echo yes | terraform login spacelift.io"
 abbr --add vpcssh ssh -F none -i ~/.ssh/aws_keys/id_ops_key -l ubuntu
-abbr --add tflogin terraform login spacelift.io
